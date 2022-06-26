e7a6cfc6c35006d07ac1e1e73f4a394061ae496f
VaishnavyVS <vaishnasubram@gmail.com>
2022-06-27 02:48:18 +0530

    review

--- src/views/GetStarted/GetStarted.js
+++ src/views/GetStarted/GetStarted.js
@@ -10,66 +10,79 @@ const GetStarted = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="GetStarted">
-      <div className="l">
-        <div className="firstline">
-          Everything you need to speed up your delivery operation
-          <div className="para">
-            {" "}
-            <div className="first">
-              <p>
-                Powerful and tested on the field features for your customers,
-                and all your teams including admins, delivery hub staff, and
-                delivery drivers
-              </p>
+    <div className="GetStartedinside">
+      <div className="GetStarted">
+        <div className="l">
+          <div className="firstline">
+            Everything you need to speed up your delivery operation
+            <div className="para">
+              {" "}
+              <div className="first">
+                <p>
+                  Powerful and tested on the field features for your customers,
+                  and all your teams including admins, delivery hub staff, and
+                  delivery drivers
+                </p>
+              </div>
             </div>
+            <Button
+              variant="contained"
+              onClick={() => navigate("/login")}
+              sx={{
+                backgroundColor: "#001E3C",
+                maxWidth: "50%",
+                alignItems: "center",
+                textAlign: "center",
+                flexDirection: "column",
+                justifyContent: "center",
+                height: 50,
+                marginTop: "3%",
+              }}
+            >
+              Get Started
+            </Button>
+          </div>
+        </div>
+        <div className="r">
+          <div className="get">
+            <div>
+              <img src={Getstarted} alt="logo" height={700} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="review">
+        <div className="reviewheader">
+          <h1> Reviews </h1>
+          <div className="para2">
+            Your review will be publically posted on the web. Use the below
+            button to create a new review to our page. Feel free to express your
+            opinions
           </div>
           <Button
             variant="contained"
-            onClick={() => navigate("/login")}
+            onClick={() => navigate("/review")}
             sx={{
               backgroundColor: "#001E3C",
-              maxWidth: "50%",
+              minWidth: "30%",
               alignItems: "center",
               textAlign: "center",
-              flexDirection: "column",
               justifyContent: "center",
+              alignSelf: "center",
               height: 50,
-              marginTop: "3%",
+              margin: "40px",
             }}
+            endIcon={<DoubleArrowIcon />}
           >
-            Get Started
+            Add Review
           </Button>
         </div>
-      </div>
-      <div className="r">
-        <div className="get">
-          <div>
-          <img src={Getstarted} alt="logo" height={650} /></div>
-          <div>
-        <Button
-          variant="contained"
-          onClick={() => navigate("/review")}
-          sx={{
-            backgroundColor: "#112c48",
-            marginRight: 7,
-            padding: 3,
-            gap: 1,
-            height: 30,
-            borderRadius: 1,
-            float: "right",
-            marginTop: 5
-          }}
-          endIcon={<DoubleArrowIcon />}
-        >
-          Add Review
-        </Button>
-      </div>
+        <div className="footer">
+          <div className="footerX">
+            <p>&copy;2022 Interpreters</p>
+          </div>
         </div>
-        
       </div>
-      
-   
     </div>
   );
 };
